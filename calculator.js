dcc1be2b76cee3d762e917a5cb28b52aecb7e2d7
Elliot Baik <elbaik412@gmail.com>
2022-05-17 15:54:33 -0500

    Create operate function

--- calculator.js
+++ calculator.js
@@ -15,7 +15,18 @@ function divide(a,b) {
 };
 
 function operate(operator, num1, num2) {
-}
+    if (operator === 'add' || operator === '+') {
+        return add(num1, num2);
+    } else if (operator === 'subtract' || operator === '-') {
+        return subtract(num1, num2);
+    } else if (operator === 'multiply' || operator === '*') {
+        return multiply(num1, num2);
+    } else if (operator === 'divide' || operator === '/') {
+        return divide(num1, num2);
+    } else {
+        return "Operation not recognized";
+    };
+};
 
 function clearAll() {}
 
