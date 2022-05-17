29373637c6ffe3403bd8cb31e5ba2d365f0ec917
Elliot Baik <elbaik412@gmail.com>
2022-05-17 15:27:35 -0500

    Create initial page and basic functions

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Calculator
+This is a calculator project as part of The Odin Project's curriculum
---
+++ calculator.js
@@ -0,0 +1,22 @@
+function add(a,b) {
+    return a + b;
+};
+
+function subtract(a,b) {
+    return a - b;
+};
+
+function multiply(a,b) {
+    return a * b;
+};
+
+function divide(a,b) {
+    return a / b;
+};
+
+function operate(operator, num1, num2) {
+}
+
+function clearAll() {}
+
+function clearNumber() {}
